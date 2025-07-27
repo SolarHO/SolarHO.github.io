0c2ee22933f69c8bbf10f92bcf52c58d59e4e2ff
SolarHO <goodus0403@naver.com>
2025-07-27 23:35:29 +0900

    Add new project images and update components

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -8,21 +8,19 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title style={{fontWeight: "bold"}}>{props.title}</Card.Title>
+        <Card.Title style={{ fontWeight: "bold" }}>{props.title}</Card.Title>
         <Card.Text className="purple">Technology: {props.technologyUsed}</Card.Text>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.link} target="_blank"className="download-button">
+        
+        <Button variant="primary" href={props.link} target="_blank" className="download-button">
           <BiLinkExternal /> &nbsp;
           Go Github
-        </Button>
-        <Button variant="primary" href={props.Article_link} target="_blank"className="download-button">
-          <BiLinkExternal /> &nbsp;
-          Article
         </Button>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
